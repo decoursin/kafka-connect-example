4ac9d512e52fc1ab0b821b03120caab3e7e9ce46
Nick <ndecoursin@gmail.com>
2017-07-26 22:28:56 +0200

    postgres, no password

--- create-connector.sh
+++ create-connector.sh
@@ -8,5 +8,5 @@ echo 'attempting post..'
 # create the JDBC sink connector.
 curl -X POST \
      -H "Content-Type: application/json" \
-     --data '{"name": "'$NAME'", "config": { "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector", "tasks.max": 1, "connection.url": "jdbc:postgresql://localhost:7999/kafka?user=postgres&password=postgres", "topics": "'$TOPICS'", "poll.interval.ms": 1000, "auto.create": true, "auto.evolve": true } }' \
+     --data '{"name": "'$NAME'", "config": { "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector", "tasks.max": 1, "connection.url": "jdbc:postgresql://localhost:7999/kafka?user=postgres&password=", "topics": "'$TOPICS'", "poll.interval.ms": 1000, "auto.create": true, "auto.evolve": true } }' \
      http://localhost:8083/connectors
